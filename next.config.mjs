1dece53d4d50b1f69607e3803cff8d9c97a1546d
mbdyjk@gmail.com <mbdyjk@gmail.com>
2025-03-31 22:08:24 +0900

    add: public 폴더 추가 (#1)

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    formats: ['image/avif', 'image/webp'],
+    localPatterns: [
+      {
+        pathname: '/**',
+        search: '',
+      },
+    ],
+  },
+};
 
 export default nextConfig;

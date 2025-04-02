c32df25395ad08727c1bafa4d97551e2885ebe00
PureunKang <pureun0000@gmail.com>
2025-04-02 20:02:03 +0900

    feat: 샤드씨엔 유아이 dialog 기본 디자인 임포트 (#10)

--- src/components/features/place-modal.tsx
+++ src/components/features/place-modal.tsx
@@ -1,7 +1,28 @@
 import React from 'react';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
 
 const PlaceModal = () => {
-  return <div>place-modal</div>;
+  return (
+    <Dialog>
+      <DialogTrigger>Open</DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Are you absolutely sure?</DialogTitle>
+          <DialogDescription>
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </DialogDescription>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
 };
 
 export default PlaceModal;

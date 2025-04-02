41c8fcc07cefe041d876b7ba4dfe6ab4e1877dd6
Eletsia <minjh2749@gmail.com>
2025-04-02 19:43:43 +0900

    feat: search-card 추가 (#7)

---
+++ src/components/features/search-card.tsx
@@ -0,0 +1,23 @@
+import Image from 'next/image';
+import { Card, CardContent, CardHeader, CardTitle } from '../ui/card';
+
+type categoryCardType = {
+  image: string;
+  title: string;
+  className: string;
+};
+const SearchCard = ({ image, title, className }: categoryCardType) => {
+  return (
+    <Card className={className}>
+      <CardHeader>
+        <CardTitle>{title}</CardTitle>
+        {/* 북마크 버튼 들어갈 자리 */}
+      </CardHeader>
+      <CardContent>
+        <Image src={image} alt={title} width={256} height={256} />
+      </CardContent>
+    </Card>
+  );
+};
+
+export default SearchCard;

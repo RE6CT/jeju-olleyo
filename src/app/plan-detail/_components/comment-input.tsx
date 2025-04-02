6848daa054cb517f11ec6ff150009685d04b6891
sohxxny <sohxxny@gmail.com>
2025-04-02 15:40:01 +0900

    feat: 댓글 등록 버튼 이벤트 핸들러 추가 (#14)

--- src/app/plan-detail/_components/comment-input.tsx
+++ src/app/plan-detail/_components/comment-input.tsx
@@ -8,14 +8,21 @@ import { Input } from '@/components/ui/input';
  * @returns
  */
 const CommentInput = () => {
-  const handleRegisterButtonClick = () => {
-    // 댓글 등록 로직
+  const user = ''; // 유저의 uuid 불러오는 로직 추후 추가
+
+  /** 댓글 등록 버튼 이벤트 핸들러 */
+  const handleRegisteCommentrButtonClick = () => {
+    if (!user) {
+      alert('로그인이 필요합니다.');
+      return;
+    }
+    // 댓글 등록 함수 호출
   };
 
   return (
     <div className="flex">
       <Input placeholder="댓글을 남겨주세요!" />
-      <Button onClick={handleRegisterButtonClick}>등록</Button>
+      <Button onClick={handleRegisteCommentrButtonClick}>등록</Button>
     </div>
   );
 };

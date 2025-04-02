7e20c0a77364dc325289d59f83b167fd28416005
mbdyjk@gmail.com <mbdyjk@gmail.com>
2025-04-02 15:08:24 +0900

    cleanup: 컨벤션에 맞게 코드 포맷팅 (#12)

--- src/app/maptest/page.tsx
+++ src/app/maptest/page.tsx
@@ -2,10 +2,10 @@
 
 import KakaoMap from '@/components/features/map/kakao-map';
 
-export default function MapPage() {
-  return (
-    <div className="h-screen w-full">
-      <KakaoMap center={{ lat: 33.45, lng: 126.57 }} level={3} />
-    </div>
-  );
-}
+const MapPage = () => (
+  <div className="h-screen w-full">
+    <KakaoMap center={{ lat: 33.45, lng: 126.57 }} level={3} />
+  </div>
+);
+
+export default MapPage;

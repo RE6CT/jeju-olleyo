3c63420c2cd3a9faf5b3fe0edd203c1229d043a1
PureunKang <pureun0000@gmail.com>
2025-04-03 17:23:35 +0900

    chore: 중간 저장 (#33)

--- src/components/layouts/search-bar-test.tsx
+++ src/components/layouts/search-bar-test.tsx
@@ -1,10 +1,34 @@
+'use client';
+
 import { Command, CommandInput } from '@/components/ui/command';
+import { useRouter } from 'next/navigation';
+
+import { useState } from 'react';
 
 const SearchBarTest = () => {
+  const [query, setQuery] = useState('');
+  const router = useRouter();
+
+  const handleSearch = () => {
+    if (query) {
+      router.push(`searchtest?query=${query}`);
+    }
+  };
+
   return (
     <>
       <Command className="w-[300px] rounded-lg border">
-        <CommandInput placeholder="검색어를 입력해 주세요" />
+        <CommandInput
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          onKeyDown={(e) => {
+            if (e.key === 'Enter') {
+              e.preventDefault();
+              handleSearch();
+            }
+          }}
+          placeholder="검색어를 입력해 주세요"
+        />
       </Command>
     </>
   );

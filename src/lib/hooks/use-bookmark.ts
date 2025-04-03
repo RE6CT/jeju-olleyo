1ddf8908ea86f0c8e7eee9a01f4f4f41f15d131f
PureunKang <pureun0000@gmail.com>
2025-04-03 12:37:14 +0900

    feat: 북마크 관련 파일 생성 (#9)

---
+++ src/lib/hooks/use-bookmark.ts
@@ -0,0 +1,28 @@
+'use client';
+
+import { useState } from 'react';
+import getLike from '@/lib/apis/get-like.api';
+import addLike from '@/lib/apis/add-like.api';
+import deleteLike from '@/lib/apis/delete-like.api';
+import countLike from '@/lib/apis/count-like.api';
+
+const useLike = (planId: number, userId: string, initialLikes: number) => {
+  const [likes, setLikes] = useState(initialLikes);
+
+  const toggleLike = async () => {
+    const currentLike = await getLike(planId, userId);
+
+    if (currentLike) {
+      await deleteLike(currentLike.plan_like_id);
+    } else {
+      await addLike(planId, userId);
+    }
+
+    const updatedCount = await countLike(planId);
+    setLikes(updatedCount != null ? updatedCount : likes);
+  };
+
+  return { likes, toggleLike };
+};
+
+export default useLike;

061edd22f5fc14e6059776d764d1a29a52fbddd9
PureunKang <pureun0000@gmail.com>
2025-04-03 12:46:49 +0900

    feat: 북마크 추가 함수 (#9)

--- src/lib/apis/bookmark/add-bookmark.api.ts
+++ src/lib/apis/bookmark/add-bookmark.api.ts
@@ -1,6 +1,6 @@
 'use server';
 
-import { createClient } from '../supabase/server';
+import { createClient } from '@/lib/supabase/server';
 
 const fetchAddBookmarkByIdQuery = async (place: number, user_id: string) => {
   const supabase = await createClient();

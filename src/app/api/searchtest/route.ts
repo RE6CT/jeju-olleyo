5c4ebfd3983e1541f3180e8d4775f107b50da6f1
PureunKang <pureun0000@gmail.com>
2025-04-04 00:14:31 +0900

    refactor: 에러 메세지 수정 (#33)

--- src/app/api/searchtest/route.ts
+++ src/app/api/searchtest/route.ts
@@ -4,6 +4,7 @@ import {
   KOREA_TOUR_APP_NAME,
   KOREA_TOUR_BASE_URL,
 } from '@/constants/\bkorea-tour-api';
+import { ApiError } from 'next/dist/server/api-utils';
 import { NextResponse, NextRequest } from 'next/server';
 
 export async function GET(req: NextRequest) {
@@ -13,7 +14,13 @@ export async function GET(req: NextRequest) {
   const areaCode = AREA_CODE_JEJU;
   const appName = encodeURIComponent(KOREA_TOUR_APP_NAME);
   const apiKey = encodeURIComponent(KOREA_TOUR_API_KEY);
+  if (!areaCode || !appName || !apiKey) {
+    throw new ApiError(500, 'API 설정 누락');
+  }
   const baseUrl = KOREA_TOUR_BASE_URL;
+  if (!baseUrl) {
+    throw new ApiError(500, 'API endpoint URL 설정이 잘못되었습니다.');
+  }
   const url = `${baseUrl}?numOfRows=100&MobileOS=ETC&MobileApp=${appName}&areaCode=${areaCode}&_type=json&serviceKey=${apiKey}`;
 
   // 지역기반 관광정보조회 (한국관광공사_국문 관광정보 서비스_GW) - 전체검색
@@ -21,13 +28,13 @@ export async function GET(req: NextRequest) {
     const res = await fetch(url);
     if (!res.ok) {
       const errorText = await res.text();
-      throw new Error(`api 호출 실패: ${res.status} => ${errorText}`);
+      throw new ApiError(res.status, `공공 API 요청 실패: ${errorText}`);
     }
 
     const data = await res.json();
     const rawData = data?.response?.body?.items?.item;
     if (!rawData) {
-      throw new Error('데이터가 없음');
+      throw new ApiError(404, '장소 데이터가 없습니다.');
     }
 
     const placeInfo = Array.isArray(rawData)

f9efac233713bb99033ba0b8842b032cc5f21cea
PureunKang <pureun0000@gmail.com>
2025-04-04 00:19:05 +0900

    chore: 헤더에 가려진거 수정 (#33)

--- src/app/searchtest/page.tsx
+++ src/app/searchtest/page.tsx
@@ -24,21 +24,23 @@ const SearchTestPage = () => {
   }, [query]);
   return (
     <>
-      <div>검색 테스트 페이지 입니다.</div>
-      <SearchBarTest />
-      <div>검색 결과는 아래와 같다.</div>
-      <ul>
-        {Array.isArray(results) ? (
-          results.map((place: any) => (
-            <div key={place.contentid}>
-              <li>{place.title}</li>
-              <li>{place.addr1}</li>
-            </div>
-          ))
-        ) : (
-          <div>검색 결과를 불러오는 중 오류가 발생했습니다.</div>
-        )}
-      </ul>
+      <div className="pt-20">
+        검색 테스트 페이지 입니다.
+        <SearchBarTest />
+        <div>검색 결과는 아래와 같다.</div>
+        <ul>
+          {Array.isArray(results) ? (
+            results.map((place: any) => (
+              <div key={place.contentid}>
+                <li>{place.title}</li>
+                <li>{place.addr1}</li>
+              </div>
+            ))
+          ) : (
+            <div>검색 결과를 불러오는 중 오류가 발생했습니다.</div>
+          )}
+        </ul>
+      </div>
     </>
   );
 };

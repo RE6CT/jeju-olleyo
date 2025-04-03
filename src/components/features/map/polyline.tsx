a2f0903b3657e015b42fc03a757be797a907cebe
mbdyjk@gmail.com <mbdyjk@gmail.com>
2025-04-03 20:09:26 +0900

    feat: 경로 관련 컴포넌트 기본 구현 (#12)

---
+++ src/components/features/map/polyline.tsx
@@ -0,0 +1,43 @@
+'use client';
+
+import { useEffect, useRef } from 'react';
+import { PolylineInstance, PolylineProps } from '@/types/kakao-map.type';
+
+const Polyline = ({ map, path, ...options }: PolylineProps) => {
+  const polylineInstance = useRef<PolylineInstance | null>(null);
+
+  // 경로 업데이트
+  useEffect(() => {
+    if (!map) return;
+
+    if (polylineInstance.current) {
+      polylineInstance.current.setMap(null);
+    }
+
+    const polyline = new window.kakao.maps.Polyline({
+      path: path.map(
+        (point) => new window.kakao.maps.LatLng(point.lat, point.lng),
+      ),
+      // default 스타일
+      strokeWeight: 3,
+      strokeColor: '#FF6B6B',
+      strokeOpacity: 0.8,
+      strokeStyle: 'solid',
+      ...options,
+    });
+
+    polyline.setMap(map);
+
+    polylineInstance.current = polyline;
+
+    return () => {
+      if (polylineInstance.current) {
+        polylineInstance.current.setMap(null);
+      }
+    };
+  }, [map, path, options]);
+
+  return null;
+};
+
+export default Polyline;

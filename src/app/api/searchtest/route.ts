f13d752ac9cad18f129e947f43bf8f5e5e1ee3bd
PureunKang <pureun0000@gmail.com>
2025-04-03 23:32:07 +0900

    fix: api 응답형식 json으로 변경 (#33)

--- src/app/api/searchtest/route.ts
+++ src/app/api/searchtest/route.ts
@@ -5,16 +5,30 @@ export async function GET(req: NextRequest) {
   const keyword = searchParams.get('query');
 
   const areaCode = '39';
-  const apiKey = process.env.NEXT_PUBLIC_KOREA_TOUR_API_KEY;
+  const appName = encodeURIComponent('제주올레요');
+  const apiKey = encodeURIComponent(
+    process.env.NEXT_PUBLIC_KOREA_TOUR_API_KEY!,
+  );
   const baseUrl = 'https://apis.data.go.kr/B551011/KorService1/areaBasedList1';
-  const url = `${baseUrl}?MobileOS=ETC&MobileApp=%EC%A0%9C%EC%A3%BC%EC%98%AC%EB%A0%88%EC%9A%94&areaCode=${areaCode}&serviceKey=${apiKey}`;
+  const url = `${baseUrl}?numOfRows=100&MobileOS=ETC&MobileApp=${appName}&areaCode=${areaCode}&_type=json&serviceKey=${apiKey}`;
 
   // 지역기반 관광정보조회 (한국관광공사_국문 관광정보 서비스_GW) - 전체검색
   try {
     const res = await fetch(url);
+    if (!res.ok) {
+      const errorText = await res.text();
+      throw new Error(`api 호출 실패: ${res.status} => ${errorText}`);
+    }
+
     const data = await res.json();
-    console.log(data);
-    const placeInfo = Array.isArray(data) ? data : Object.values(data ?? {});
+    const rawData = data?.response?.body?.items?.item;
+    if (!rawData) {
+      throw new Error('데이터가 없음');
+    }
+
+    const placeInfo = Array.isArray(rawData)
+      ? rawData
+      : Object.values(rawData ?? {});
 
     const filteredPlace = placeInfo.filter((p: any) => {
       return p.title?.includes(keyword);

b698aa17d55a3ce92aff810330369b0841e549d8
mbdyjk@gmail.com <mbdyjk@gmail.com>
2025-04-03 09:29:12 +0900

    feat: 마커 관련 타입 정의 추가 (#12)

--- src/types/kakao-map.type.ts
+++ src/types/kakao-map.type.ts
@@ -12,3 +12,21 @@ export type KakaoMapInstance = {
   getCenter(): { getLat(): number; getLng(): number };
   getLevel(): number;
 };
+
+export type MarkerOptions = {
+  position: {
+    lat: number;
+    lng: number;
+  };
+  title: string;
+  clickable: boolean;
+  draggable: boolean;
+};
+
+export type MarkerInstance = {
+  setMap(map: KakaoMapInstance | null): void;
+  setPosition(position: { getLat(): number; getLng(): number }): void;
+  setTitle(title: string): void;
+  setClickable(clickable: boolean): void;
+  setDraggable(draggable: boolean): void;
+};

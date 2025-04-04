70dbc2be1f791db696b5dc94c468e5e1fa154b39
Eletsia <minjh2749@gmail.com>
2025-04-04 18:33:14 +0900

    fix: 코드 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,3 @@
-import CategoryCard from '@/components/features/category-card';
-import PlanCard from '@/components/features/plan-card';
-import SearchCard from '@/components/features/search-card';
-
 export default function Home() {
-  return <></>;
+  return <div></div>;
 }

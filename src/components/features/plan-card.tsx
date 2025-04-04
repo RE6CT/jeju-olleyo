0cc395610be07a3f97179387e1ebd3108e3f07a8
Eletsia <minjh2749@gmail.com>
2025-04-04 18:20:04 +0900

    fix: 타입 변경경

--- src/components/features/plan-card.tsx
+++ src/components/features/plan-card.tsx
@@ -6,23 +6,43 @@ import {
   CardHeader,
   CardTitle,
 } from '../ui/card';
+import { Tables } from '@/types/supabase.type';
+import Link from 'next/link';
+import LikeButton from './like-button';
+import { CamelCaseObject } from '@/types/common.type';
 
+type Plan = Tables<'plans'>;
 type PlanCardProp = {
-  image: string;
-  title: string;
-  description: string;
-  className: string;
+  plan: CamelCaseObject<
+    Pick<Plan, 'plan_img' | 'title' | 'description' | 'plan_id'>
+  >;
+  className?: string;
+  userId: string;
+  initialLikes: number;
 };
-const PlanCard = ({ image, title, description, className }: PlanCardProp) => {
+const PlanCard = ({ plan, className, userId, initialLikes }: PlanCardProp) => {
   return (
     <Card className={className}>
-      <Image src={image} alt={title} width={256} height={256} />
+      <Link href={`/plan-detail/${plan.planId}`}>
+        {plan.planImg && (
+          <Image
+            src={plan.planImg}
+            alt={plan.title ?? 'Plan Image'}
+            width={256}
+            height={256}
+          />
+        )}
+      </Link>
       <CardHeader>
-        <CardTitle>{title}</CardTitle>
-        <CardDescription>{description}</CardDescription>
+        <CardTitle>{plan.title}</CardTitle>
+        <CardDescription>{plan.description}</CardDescription>
       </CardHeader>
       <CardContent></CardContent>
-      {/* 좋아요 버튼 들어갈 자리 */}
+      <LikeButton
+        userId={userId}
+        planId={plan.planId}
+        initialLikes={initialLikes}
+      />
     </Card>
   );
 };
--- src/components/features/search-card.tsx
+++ src/components/features/search-card.tsx
@@ -1,18 +1,39 @@
 import Image from 'next/image';
 import { Card, CardContent, CardHeader, CardTitle } from '../ui/card';
+import { CamelCaseObject } from '@/types/common.type';
+import { Tables } from '@/types/supabase.type';
+import Bookmark from './bookmark';
 
-type categoryCardType = {
+type bookmarks = Tables<'bookmarks'>;
+type SearchCardProp = {
+  bookmarks: CamelCaseObject<
+    Pick<bookmarks, 'user_id' | 'place' | 'place_lat' | 'place_lng'>
+  >;
+  className?: string;
+  initialBookmarks: boolean;
   image: string;
   title: string;
-  className: string;
 };
-const SearchCard = ({ image, title, className }: categoryCardType) => {
+
+const SearchCard = ({
+  bookmarks,
+  className,
+  initialBookmarks,
+  image,
+  title,
+}: SearchCardProp) => {
   return (
     <Card className={className}>
       <Image src={image} alt={title} width={256} height={256} />
       <CardHeader>
         <CardTitle>{title}</CardTitle>
-        {/* 북마크 버튼 들어갈 자리 */}
+        <Bookmark
+          userId={bookmarks.userId}
+          place={bookmarks.place ? bookmarks.place : 0}
+          initialBookmarks={initialBookmarks}
+          place_lat={bookmarks.placeLat}
+          place_lng={bookmarks.placeLng}
+        />
       </CardHeader>
       <CardContent></CardContent>
     </Card>

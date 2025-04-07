c71e30901e75fc6f5055beccfe7c4df9cc702783
PureunKang <pureun0000@gmail.com>
2025-04-07 21:23:10 +0900

    move: 주석을 타입 파일로 이동 (#10)

--- src/types/plan-card-modal.type.ts
+++ src/types/plan-card-modal.type.ts
@@ -1,4 +1,20 @@
 // 플랜 카드 모달창 props 타입
+/**
+ *
+ * @param open - 모달
+ * @param onOpenChange - 열림 상태 바꾸는 함수
+ * @param mode - 모달 타입 ("success" | "isBack" | "select" | "isDelete")
+ * @param onConfirm - 확인 버튼 시 실행될 함수
+ * @param onCancel - 취소 버튼 시 실행될 함수
+ */
+
+/**
+ * 모달의 타입입니다.
+ * - "success": 일정 생성 완료
+ * - "isBack": 되돌아가기 경고
+ * - "select": 날짜 선택 안내
+ * - "isDelete": 전체 삭제 확인
+ */
 export type PlanCardModalProps = {
   open: boolean;
   onOpenChange: (open: boolean) => void;

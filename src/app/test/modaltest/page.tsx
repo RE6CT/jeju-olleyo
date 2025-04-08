0e1f0151ea425db3d5cf7f11eb6f0304b29c9f39
PureunKang <pureun0000@gmail.com>
2025-04-08 20:02:59 +0900

    feat: 모달 테스트 페이지 (#10)

--- src/app/test/modaltest/page.tsx
+++ src/app/test/modaltest/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import PlaceModal from '@/components/features/place-modal';
 import PlanCardModal from '@/components/features/plan/plan-card-modal';
 import { useState } from 'react';
 
@@ -15,21 +16,34 @@ const TestPage = () => {
   };
   return (
     <>
-      <div className="flex gap-4 pt-20">
-        <button onClick={() => handleOpenModal('success')}>저장하기</button>
-        <button onClick={() => handleOpenModal('isBack')}>돌아가기</button>
-        <button onClick={() => handleOpenModal('select')}>날짜선택 모달</button>
-        <button onClick={() => handleOpenModal('isDelete')}>삭제</button>
-      </div>
-
       <div>
-        <PlanCardModal
-          open={modalOpen}
-          onOpenChange={setModalOpen}
-          mode={mode}
-          onConfirm={() => setModalOpen(false)}
-          onCancel={() => setModalOpen(false)}
-        />
+        플레이스모달테스트
+        <div>
+          <PlaceModal />
+        </div>
+      </div>
+      <div>
+        알람모달테스트
+        <div>
+          <div className="flex gap-4 pt-20">
+            <button onClick={() => handleOpenModal('success')}>저장하기</button>
+            <button onClick={() => handleOpenModal('isBack')}>돌아가기</button>
+            <button onClick={() => handleOpenModal('select')}>
+              날짜선택 모달
+            </button>
+            <button onClick={() => handleOpenModal('isDelete')}>삭제</button>
+          </div>
+
+          <div>
+            <PlanCardModal
+              open={modalOpen}
+              onOpenChange={setModalOpen}
+              mode={mode}
+              onConfirm={() => setModalOpen(false)}
+              onCancel={() => setModalOpen(false)}
+            />
+          </div>
+        </div>
       </div>
     </>
   );

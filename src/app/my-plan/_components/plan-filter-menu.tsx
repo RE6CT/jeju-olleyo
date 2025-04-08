150ada2c77d7503cf924d8d0e576d35aa412c242
mbdyjk@gmail.com <mbdyjk@gmail.com>
2025-04-08 13:03:15 +0900

    refactor: onClick 핸들러 함수 분리하여 정의 (#48)

--- src/app/my-plan/_components/plan-filter-menu.tsx
+++ src/app/my-plan/_components/plan-filter-menu.tsx
@@ -1,6 +1,29 @@
 import { Button } from '@/components/ui/button';
 import { FILTER_TYPES } from '@/constants/plan.constants';
-import { FilterMenuProps } from '@/types/plan.type';
+import { FilterMenuProps, FilterState, FilterType } from '@/types/plan.type';
+
+/**
+ * 필터 버튼 클릭 핸들러
+ * @param filterType - 선택된 필터 타입 (제목, 날짜, 공개상태)
+ * @param setSelectedFilter - 선택된 필터를 설정하는 함수
+ * @param setInputValue - 입력값을 설정하는 함수
+ * @param filter - 현재 적용된 필터 상태
+ */
+const handleFilterClick = (
+  filterType: FilterType,
+  setSelectedFilter: (filter: FilterType) => void,
+  setInputValue: (value: string) => void,
+  filter: FilterState,
+) => {
+  setSelectedFilter(filterType);
+  setInputValue(
+    filterType === FILTER_TYPES.PUBLIC
+      ? ''
+      : filter.type === filterType
+        ? filter.value
+        : '',
+  );
+};
 
 export const FilterMenu = ({
   selectedFilter,
@@ -12,30 +35,42 @@ export const FilterMenu = ({
     <Button
       variant={selectedFilter === FILTER_TYPES.TITLE ? 'default' : 'ghost'}
       className="w-full justify-start"
-      onClick={() => {
-        setSelectedFilter(FILTER_TYPES.TITLE);
-        setInputValue(filter.type === FILTER_TYPES.TITLE ? filter.value : '');
-      }}
+      onClick={() =>
+        handleFilterClick(
+          FILTER_TYPES.TITLE,
+          setSelectedFilter,
+          setInputValue,
+          filter,
+        )
+      }
     >
       제목
     </Button>
     <Button
       variant={selectedFilter === FILTER_TYPES.DATE ? 'default' : 'ghost'}
       className="w-full justify-start"
-      onClick={() => {
-        setSelectedFilter(FILTER_TYPES.DATE);
-        setInputValue(filter.type === FILTER_TYPES.DATE ? filter.value : '');
-      }}
+      onClick={() =>
+        handleFilterClick(
+          FILTER_TYPES.DATE,
+          setSelectedFilter,
+          setInputValue,
+          filter,
+        )
+      }
     >
       날짜
     </Button>
     <Button
       variant={selectedFilter === FILTER_TYPES.PUBLIC ? 'default' : 'ghost'}
       className="w-full justify-start"
-      onClick={() => {
-        setSelectedFilter(FILTER_TYPES.PUBLIC);
-        setInputValue('');
-      }}
+      onClick={() =>
+        handleFilterClick(
+          FILTER_TYPES.PUBLIC,
+          setSelectedFilter,
+          setInputValue,
+          filter,
+        )
+      }
     >
       공개상태
     </Button>

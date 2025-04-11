866a5fc0b023dd2b5e20d5b829718851ea27e6dd
soheun <sohxxny@gmail.com>
2025-04-11 20:04:41 +0900

    feat: 이미지 추가 및 코드 수정 (#37)

--- src/lib/utils/get-image-with-fallback.ts
+++ src/lib/utils/get-image-with-fallback.ts
@@ -10,17 +10,17 @@ export const getProfileImageWithFallback = (image: string | null): string => {
 /**
  * 장소 이미지가 null일 경우 기본 이미지를 띄우는 함수
  * @param image - 이미지 문자열
- * @returns 이미지 문자열 존재할 경우 해당 문자열, 아닐 경우 /default_place_image.png
+ * @returns 이미지 문자열 존재할 경우 해당 문자열, 아닐 경우 /default_place_image.svg
  */
 export const getPlaceImageWithFallback = (image: string | null): string => {
-  return image ? image : '/images/default_place_image.png';
+  return image ? image : '/images/default_place_image.svg';
 };
 
 /**
  * 일정 이미지가 null일 경우 기본 이미지를 띄우는 함수
  * @param image - 이미지 문자열
- * @returns 이미지 문자열 존재할 경우 해당 문자열, 아닐 경우 /default_plan_image.png
+ * @returns 이미지 문자열 존재할 경우 해당 문자열, 아닐 경우 /default_plan_image.svg
  */
 export const getPlanImageWithFallback = (image: string | null): string => {
-  return image ? image : '/images/default_plan_image.png';
+  return image ? image : '/images/default_plan_image.svg';
 };

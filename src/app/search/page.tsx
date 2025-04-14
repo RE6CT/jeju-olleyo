1dfc3fe4b49ec05cbfea2f5ec50e1a7cfea6e804
PureunKang <pureun0000@gmail.com>
2025-04-14 17:50:14 +0900

    feat: 카테고리 필터링 (#112)

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -6,32 +6,57 @@ import PlaceImage from '@/components/commons/place-image';
 import useSearch from '@/lib/hooks/use-search';
 import Loading from '../loading';
 import EmptyResult from './_components/empty-result';
+import { useState } from 'react';
 
 // TODO : 카드 컴포넌트 머지 후 div 요소 교체 필요 - 링크, 좋아요, 북마크 다 삽입될 예정
 // TODO : 디자이너님 배너 제작 후 배너 삽입 예정
 // TODO : 검색 중 상태일 때 사용될 수 있는 이미지 있는지 디자이너님께 문의 필요.
+
+const CATEGORIES = ['전체', '명소', '숙박', '맛집', '카페'];
 
 const SearchResultsPage = () => {
   const searchParams = useSearchParams();
   const query = searchParams.get('query') ?? '';
   const { results, loading } = useSearch(query);
 
+  const [activeCategory, setActiveCategory] = useState<string>('전체');
+
   if (loading) {
     return <Loading />;
   }
 
+  const filteredResults =
+    activeCategory === '전체'
+      ? results
+      : results.filter((place) => place.category === activeCategory);
+
   return (
     <div className="px-4">
       <div className="mb-6 text-2xl font-bold">'{query}'의 검색 결과</div>
+      <div className="mb-[17px] mt-5 space-x-3">
+        {CATEGORIES.map((cat) => (
+          <button
+            key={cat}
+            onClick={() => setActiveCategory(cat)}
+            className={`rounded-md px-4 py-2 ${
+              activeCategory === cat
+                ? 'bg-black text-white'
+                : 'bg-gray-100 text-gray-700'
+            }`}
+          >
+            {cat}
+          </button>
+        ))}
+      </div>
 
-      {results.length === 0 ? (
+      {filteredResults.length === 0 ? (
         <EmptyResult />
       ) : (
         (() => {
           const grouped: JSX.Element[] = [];
 
-          for (let i = 0; i < results.length; i += 8) {
-            const slice = results.slice(i, i + 8);
+          for (let i = 0; i < filteredResults.length; i += 8) {
+            const slice = filteredResults.slice(i, i + 8);
 
             grouped.push(
               <ul key={`group-${i}`} className="mb-6 grid grid-cols-4 gap-4">
--- src/lib/hooks/use-search.ts
+++ src/lib/hooks/use-search.ts
@@ -4,11 +4,14 @@ import { useEffect, useState } from 'react';
 import Fuse from 'fuse.js';
 import fetchGetAllPlaces from '../apis/search/get-place.api';
 import { Place } from '@/types/search.type';
+import { useRouter } from 'next/navigation';
 
 const useSearch = (query: string) => {
   const [allPlaces, setAllPlaces] = useState<Place[]>([]);
   const [results, setResults] = useState<Place[]>([]);
   const [loading, setLoading] = useState(true);
+
+  const router = useRouter();
 
   useEffect(() => {
     const fetchPlaces = async () => {
@@ -25,12 +28,10 @@ const useSearch = (query: string) => {
     fetchPlaces();
   }, []);
 
-  // 일단 카테고리별 페이지 없는 상태라서 전체 데이터 로딩
-  // TODO : 카테고리별 페이지 만들면, 검색어 없을 경우, '전체' 카테고리 페이지로 리다이렉트 예정
+  // 검색어 없을 경우, '전체' 카테고리 페이지로 리다이렉트
   useEffect(() => {
     if (!query.trim()) {
-      setResults(allPlaces);
-      return;
+      router.replace('/categories/all');
     }
 
     const fuse = new Fuse(allPlaces, {

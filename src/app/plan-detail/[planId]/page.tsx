d59754168c90d6d6e5a0a27b722a3f0348633aea
mbdyjk@gmail.com <mbdyjk@gmail.com>
2025-04-17 06:52:53 +0900

    add: 일정 상세 페이지 추가 및 페이지 라우팅,  로그인 체크 구현 (#152)

--- src/app/plan-detail/[planId]/page.tsx
+++ src/app/plan-detail/[planId]/page.tsx
@@ -1,7 +1,24 @@
-const PlanDetailPage = ({ params }: { params: { planId: string } }) => {
-  const planId = parseInt(params.planId);
+import { fetchGetCurrentUser } from '@/lib/apis/auth/auth-server.api';
+import ErrorMessage from '@/components/features/alert/error-message';
 
-  return <div>MyPlanPage</div>;
+const PlanDetailPage = async ({ params }: { params: { planId: string } }) => {
+  const { user } = await fetchGetCurrentUser();
+  const userId = user?.id;
+
+  if (!userId) {
+    return (
+      <ErrorMessage
+        title="로그인 필요"
+        description="일정을 보려면 로그인이 필요합니다."
+      />
+    );
+  }
+
+  return (
+    <div className="flex flex-col">
+      <span className="text-28 font-bold">일정 보기</span>
+    </div>
+  );
 };
 
 export default PlanDetailPage;

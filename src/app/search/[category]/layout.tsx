74210a7ce622628341c3e8bb11eb477069626573
kangpureun <pureun0000@gmail.com>
2025-04-25 12:13:51 +0900

    feat: 검색 결과 페이지 메타데이터 설정 (#173)

---
+++ src/app/search/[category]/layout.tsx
@@ -0,0 +1,8 @@
+import { Metadata } from 'next';
+import SearchResultsPage from './page';
+
+export const metadata: Metadata = {
+  title: '검색 결과',
+  description: '제주도 내 장소 검색 결과를 살펴보세요!',
+};
+export default SearchResultsPage;

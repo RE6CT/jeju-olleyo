b5e98ccda77e0ccfb730acb1b6747aaf9881bed0
kangpureun <pureun0000@gmail.com>
2025-04-25 12:26:14 +0900

    chore: 검색 페이지 메타데이터 수정 (#173)

--- src/app/search/[category]/layout.tsx
+++ src/app/search/[category]/layout.tsx
@@ -2,7 +2,7 @@ import { Metadata } from 'next';
 import SearchResultsPage from './page';
 
 export const metadata: Metadata = {
-  title: '검색 결과',
-  description: '제주도 내 장소 검색 결과를 살펴보세요!',
+  title: '제주도 여행지 검색 결과',
+  description: '제주도 내 장소 검색 결과를 확인해 보세요.',
 };
 export default SearchResultsPage;

5ec7ea1fb8a33fcd9eec65ec0cfe377a45cdb541
JoYoungHyun <younghyun550@gmail.com>
2025-04-29 22:30:21 +0900

    style: 인증 페이지 레이아웃에서 하단 여백 조정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
       >
         <Providers>
           <AuthProvider>
-            <div className="mx-auto flex h-full w-full min-w-[375px] max-w-[375px] flex-col overflow-hidden bg-home-gradient pb-[86px] md:max-w-[769px] md:bg-none lg:max-w-[1024px]">
+            <div className="mx-auto flex h-full w-full min-w-[375px] max-w-[375px] flex-col overflow-hidden bg-home-gradient pb-[86px] md:max-w-[769px] md:bg-none md:pb-0 lg:max-w-[1024px]">
               <Header />
               <div className="flex h-full w-full flex-col overflow-auto md:pb-0">
                 <main className="flex-1">{children}</main>

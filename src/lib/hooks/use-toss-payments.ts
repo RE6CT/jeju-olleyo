aa9333d5fcee45e763210d027726b1c44c57f232
sohxxny <sohxxny@gmail.com>
2025-07-03 00:45:05 +0900

    feat: 결제 UI 렌더링 로직 추가 (#308)

--- src/lib/hooks/use-toss-payments.ts
+++ src/lib/hooks/use-toss-payments.ts
@@ -1,34 +1,79 @@
 import { useEffect, useState } from 'react';
-import { loadTossPayments } from '@tosspayments/tosspayments-sdk';
+import {
+  loadTossPayments,
+  TossPaymentsWidgets,
+} from '@tosspayments/tosspayments-sdk';
 
-const useTossPayments = () => {
-  const [isLoading, setIsLoading] = useState(true);
-  const [isError, setError] = useState(false);
+const clientKey = process.env.NEXT_PUBLIC_TOSS_CLIENT_KEY || '';
 
+const useTossPayments = (customerKey: string, value: number) => {
+  const [widgets, setWidgets] = useState<TossPaymentsWidgets | null>(null);
+  const [amount, setAmount] = useState({
+    currency: 'KRW',
+    value,
+  });
+  const [ready, setReady] = useState(false);
+
+  // 위젯 초기화
   useEffect(() => {
-    /**
-     * 토스 페이먼츠 SDK 초기화 함수
-     */
     const fetchPaymentWidgets = async () => {
       try {
         // SDK 초기화
-        const tossPayments = await loadTossPayments(
-          process.env.NEXT_PUBLIC_TOSS_CLIENT_KEY || '',
-        );
-      } catch (err) {
-        console.error('토스페이먼츠 SDK 초기화 중 에러 발생:', err);
-        setError(true);
-      } finally {
-        setIsLoading(false);
+        const tossPayments = await loadTossPayments(clientKey);
+
+        // 위젯 초기화
+        const widgets = tossPayments.widgets({
+          customerKey,
+        });
+
+        setWidgets(widgets);
+      } catch (error) {
+        console.error('토스페이먼츠 위젯 초기화 중 에러 발생:', error);
       }
     };
 
     fetchPaymentWidgets();
-  }, []);
+  }, [clientKey, customerKey]);
 
-  return {
-    isLoading,
-    isError,
+  // 결제 UI 렌더링
+  useEffect(() => {
+    const renderPaymentWidgets = async () => {
+      // 위젯이 로드되지 않았다면 실행하지 않기
+      if (widgets == null) {
+        return;
+      }
+
+      // 위젯의 결제 금액을 결제하려는 금액으로 초기화
+      await widgets.setAmount(amount);
+
+      // 결제 UI 렌더링
+      await widgets.renderPaymentMethods({
+        selector: '#payment-method',
+        variantKey: 'DEFAULT',
+      });
+
+      // 이용약관 UI 렌더링
+      await widgets.renderAgreement({
+        selector: '#agreement',
+        variantKey: 'AGREEMENT',
+      });
+
+      setReady(true);
+    };
+
+    renderPaymentWidgets();
+  }, [widgets]);
+
+  /**
+   * 주문서의 결제 금액이 변경되었을 경우 결제 금액을 업데이트하는 함수
+   * @param value - 바뀐 결제 금액
+   */
+  const updateAmount = async (value: number) => {
+    if (widgets === null) {
+      return;
+    }
+    setAmount({ ...amount, value });
+    await widgets.setAmount(amount);
   };
 };
 
